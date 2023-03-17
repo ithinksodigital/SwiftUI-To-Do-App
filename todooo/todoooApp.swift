f57dd41e676a16ea630e16f2388f5c37227ecd15
Rafal Pawelec (PL) <rafal.pawelec@pwc.com>
2023-03-17 22:50:43 +0100

    Initial Commit

---
+++ todooo/todoooApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoooApp.swift
+//  todooo
+//
+//  Created by Rafal Pawelec on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct todoooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
